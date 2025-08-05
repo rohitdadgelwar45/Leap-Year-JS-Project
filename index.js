65b8f81e064aa7f376c83258aabe14b4af36daa9
rohit <rohitdadgelwar123@gmail.com>
2025-08-06 02:18:14 +0530

    adding leap year project

---
+++ index.js
@@ -0,0 +1,22 @@
+
+let button_ele = document.querySelector(".btn");
+
+let outputEle=document.getElementById("output");
+
+button_ele.addEventListener("click",(event)=>{
+    let yearEle=parseInt(document.getElementById("year").value);
+    isLeapYear(yearEle);
+    document.getElementById("year").value="";
+})
+
+function isLeapYear(yearEle){
+    if(isNaN(yearEle)){
+        outputEle.innerText="Enter Valid Year.";
+    }
+    else if((yearEle%4===0) || (yearEle%400===0) && (yearEle%100!==0)){
+        outputEle.innerText=yearEle + " is a Leap Year.";
+    }
+    else{
+        outputEle.innerText=yearEle + " is not a Leap Year.";
+    }
+}
