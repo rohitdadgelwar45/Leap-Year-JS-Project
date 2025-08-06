4c02a0bd0249c2ffd7bd5036296a2b3b028328b7
rohit <rohitdadgelwar123@gmail.com>
2025-08-06 12:08:51 +0530

    update js file

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function isLeapYear(yearEle){
     if(isNaN(yearEle)){
         outputEle.innerText="Enter Valid Year.";
     }
-    else if((yearEle%4===0) || (yearEle%400===0) && (yearEle%100!==0)){
+    else if((yearEle%400===0) || ((yearEle%4===0) && (yearEle%100!==0))){
         outputEle.innerText=yearEle + " is a Leap Year.";
     }
     else{
